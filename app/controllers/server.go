4de6ce98c3063be9311ac15564ff6c5f7578ff18
kinho <kinhong99@gmail.com>
2022-08-20 16:51:48 -0400

    add handlers on server

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	controllers "github.com/k1nho/goKingdomAPI/controllers/products_ctrl"
 )
 
 var router *mux.Router
 
+func initHandlers() {
+	router.HandleFunc("/api/kingdom/products", controllers.GetProducts).Methods("GET")
+	router.HandleFunc("/api/kingdom/products/{product_id}", controllers.GetProducts).Methods("GET")
+}
+
 func RunServer() {
 	router = mux.NewRouter()
-
+	initHandlers()
 	log.Fatal(http.ListenAndServe(":8000", router))
 }

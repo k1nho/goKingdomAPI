407c01bb778b9b2710c02cf69901ec5ccee1e88a
kinho <kinhong99@gmail.com>
2022-08-21 16:34:38 -0400

    restructure

--- app/model/product.go
+++ app/model/product.go
@@ -1,15 +1,11 @@
-package products
+package model
 
-import (
-	"github.com/k1nho/goKingdomAPI/model"
-)
-
-func FetchProducts() ([]model.Product, error) {
-	var products []model.Product
-	var product model.Product
+func FetchProducts() ([]Product, error) {
+	var products []Product
+	var product Product
 
 	sqlQuery := `SELECT * FROM products;`
-	rows, err := model.DB.Query(sqlQuery)
+	rows, err := DB.Query(sqlQuery)
 
 	if err != nil {
 		return products, err
@@ -28,11 +24,11 @@ func FetchProducts() ([]model.Product, error) {
 	return products, nil
 }
 
-func FetchProduct(product_id int) (model.Product, error) {
-	var product model.Product
+func FetchProduct(product_id uint64) (Product, error) {
+	var product Product
 
 	sqlQuery := `SELECT * FROM products WHERE id=$1`
-	row := model.DB.QueryRow(sqlQuery, product_id)
+	row := DB.QueryRow(sqlQuery, product_id)
 	if row == nil {
 		return product, row.Err()
 	}
